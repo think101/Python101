fda128ef7cf54c5ee748d1589f8b5cb048d5fad5
think101 <taohuayuan2000@hotmail.com>
2022-03-01 08:16:46 -0800

    217. Contains Duplicate

---
+++ src/contains_duplicate.py
@@ -0,0 +1,12 @@
+from typing import List
+
+
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        a = set()
+        for i in nums:
+            if i in a:
+                return True
+            a.add(i)
+
+        return False
