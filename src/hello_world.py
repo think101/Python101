174b6bcafa5845e77ef0e1106effdb516aa952d1
think101 <taohuayuan2000@hotmail.com>
2022-03-01 08:04:07 -0800

    update hello world file

---
+++ src/hello_world.py
@@ -0,0 +1,36 @@
+print("Hello Python world!")
+
+message = "Hello Python world!"
+print(message)
+
+name = "ada lovelace"
+print(name.title())
+print(name.upper())
+
+first_name = "ada"
+last_name = "lovelace"
+full_name = f"full name is: {first_name} {last_name}"
+print(full_name)
+
+print("Language:\nPython\nC\nJavaScript")
+
+print(' abc python ')
+print(' abc python '.rstrip())
+print(' abc python '.lstrip())
+print(' abc python '.strip())
+
+print(0.2 + 0.1)
+print(0.2 + 0.1 == 0.3)    # False
+
+universe_age = 14_000_000_000
+print(universe_age)
+
+bicycles = ['trek', 'cannondale', 'redline', 'specialized']
+print(bicycles)
+print(bicycles[0])
+print(bicycles[-1])
+print(bicycles[-2])
+
+bicycles[1] = 'dahon'
+print(bicycles)
+
