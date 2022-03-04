bf4584a582ebb8ec44ad818af401cda9a49cb726
think101 <taohuayuan2000@hotmail.com>
2022-03-03 18:32:09 -0800

    53. Maximum Subarray minor improvement

--- src/maximum_subarray.py
+++ src/maximum_subarray.py
@@ -5,14 +5,9 @@ class Solution:
     def maxSubArray(self, nums: List[int]) -> int:
         dp = [0] * len(nums)
         dp[0] = nums[0]
-        res = nums[0]
-
         for i in range(1, len(nums)):
-            dp[i] = max(dp[i-1]+nums[i], nums[i])
-            if dp[i] > res:
-                res = dp[i]
-
-        return res
+            dp[i] = max(dp[i - 1] + nums[i], nums[i])
+        return max(dp)
 
 
 t = Solution()
