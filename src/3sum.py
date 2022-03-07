c75fd0003202c21ddd1fab47d42165330c19ae41
think101 <taohuayuan2000@hotmail.com>
2022-03-06 18:24:43 -0800

    15. 3Sum

---
+++ src/3sum.py
@@ -0,0 +1,31 @@
+import collections
+from typing import List
+
+
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        result = []
+        if len(nums) < 3:
+            return result
+
+        nums.sort()
+        counter = collections.Counter(nums)
+
+        for i in range(0, len(nums)):
+            if i - 1 >= 0 and nums[i] == nums[i - 1]:
+                continue
+
+            for j in range(i + 1, len(nums)):
+                if j - 1 != i and j - 1 >= 0 and nums[j - 1] == nums[j]:
+                    continue
+
+                t = -nums[i] - nums[j]
+                if t >= nums[j] and t in counter.keys() and counter.get(t) >= 1 + int(nums[i] == t) + int(nums[j] == t):
+                    l = [nums[i], nums[j], t]
+                    result.append(l)
+
+        return result
+
+
+t = Solution()
+print(t.threeSum([-1, 0, 1, 2, -1, -4]))
