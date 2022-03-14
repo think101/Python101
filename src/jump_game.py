51babfe4ecb5655ebc4fbab5f91a76fdc0c24b3b
think101 <taohuayuan2000@hotmail.com>
2022-03-13 19:55:27 -0700

    55. Jump Game

---
+++ src/jump_game.py
@@ -0,0 +1,24 @@
+from typing import List
+
+
+class Solution:
+    def canJump(self, nums: List[int]) -> bool:
+        dp = [0] * len(nums)
+        dp[0] = nums[0]
+
+        for i in range(1, len(nums)):
+            if dp[i-1] < i:
+                return False
+
+            dp[i] = max(dp[i-1], nums[i]+i)
+
+        return dp[len(nums)-1] >= len(nums)-1
+
+
+t = Solution()
+print(t.canJump([4,0,4,2,2,0,1,3,3,0,3]))
+print(t.canJump([2,3,1,1,4]))
+print(t.canJump([3,2,1,0,4]))
+print(t.canJump([2,0,0]))
+print(t.canJump([0]))
+
