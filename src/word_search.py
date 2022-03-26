f84c8d50e1c21705d7a18f175fff8426780d03ee
think101 <taohuayuan2000@hotmail.com>
2022-03-26 16:37:16 -0700

    79. Word Search TLE solution

--- src/word_search.py
+++ src/word_search.py
@@ -13,19 +13,19 @@ class Solution:
             if index == word_len - 1:
                 return True
 
-            v = [[0] * col] * row
+            v = [[0] * col for _ in range(row)]
             for x in range(row):
                 for y in range(col):
                     v[x][y] = visited[x][y]
 
             v[i][j] = 1
-            return dfs(index + 1, i - 1, j, v) or dfs(index + 1, i + 1, j, v) or dfs(index + 1, i, j - 1, v) or dfs(
-                index + 1, i, j + 1, v)
+            return dfs(index + 1, i - 1, j, v) or dfs(index + 1, i + 1, j, v) or \
+                   dfs(index + 1, i, j - 1, v) or dfs(index + 1, i, j + 1, v)
 
         for a in range(row):
             for b in range(col):
                 if board[a][b] == word[:1]:
-                    v = [[0] * col] * row
+                    v = [[0] * col for _ in range(row)]
                     if dfs(0, a, b, v):
                         return True
 
@@ -33,4 +33,7 @@ class Solution:
 
 
 t = Solution()
-print(t.exist([["A", "B"], ["S", "F"]], "ABF"))
+#print(t.exist([["A", "A"], ["A", "A"]], "AA"))
+print(t.exist([["A", "A", "A", "A", "A", "A"], ["A", "A", "A", "A", "A", "A"], ["A", "A", "A", "A", "A", "A"],
+               ["A", "A", "A", "A", "A", "A"], ["A", "A", "A", "A", "A", "A"], ["A", "A", "A", "A", "A", "A"]],
+              "AAAAAAAAAAAAAAB"))
