c0edf1c0e88ff71536f7cc4b27ca5d3bf2e1a9c2
think101 <taohuayuan2000@hotmail.com>
2022-03-31 19:41:14 -0700

    647. Palindromic Substrings

---
+++ src/palindromic_substrs.py
@@ -0,0 +1,25 @@
+class Solution:
+    def countSubstrings(self, s: str) -> int:
+        res = 0
+
+        for i in range(len(s)):
+            l, r = i, i
+            while l >= 0 and r < len(s) and s[l:l+1] == s[r:r+1]:
+                res += 1
+                l -= 1
+                r += 1
+
+            l, r = i, i+1
+            while l >= 0 and r < len(s) and s[l:l+1] == s[r:r+1]:
+                res += 1
+                l -= 1
+                r += 1
+
+        return res
+
+
+if __name__ == '__main__':
+    sol = Solution()
+    s = "aaa"
+    print(sol.countSubstrings(s))
+
