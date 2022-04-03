01564dc24aacb653bfb2833552ec9de5ee2fa2d0
think101 <taohuayuan2000@hotmail.com>
2022-04-03 09:28:15 -0700

    124. Binary Tree Maximum Path Sum minor change

--- src/binary_tree_max_path_sum.py
+++ src/binary_tree_max_path_sum.py
@@ -8,23 +8,32 @@ class TreeNode:
         self.right = right
 
 
+res = float('-inf')
+
+
 class Solution:
 
     def maxPathSum(self, root: Optional[TreeNode]) -> int:
         global res
         res = float('-inf')
 
-        def helper(root: Optional[TreeNode]) -> int:
+        def helper(node: Optional[TreeNode]) -> int:
             global res
 
-            if not root:
+            if not node:
                 return float('-inf')
 
-            l, r = max(0, helper(root.left)), max(0, helper(root.right))
-            res = max(res, root.val + l + r)
+            l, r = max(0, helper(node.left)), max(0, helper(node.right))
+            res = max(res, node.val + l + r)
 
-            return max(l, r) + root.val
+            return max(l, r) + node.val
 
         helper(root)
 
         return res
+
+
+t = TreeNode(1)
+t.left = TreeNode(2)
+t.right = TreeNode(3)
+print(Solution().maxPathSum(t))
