bdc5e87817a07ba5e22b58df6fa29ad4d98de512
think101 <taohuayuan2000@hotmail.com>
2022-04-09 23:46:11 -0700

    268. Missing Number

---
+++ src/missing_number.py
@@ -0,0 +1,15 @@
+from typing import List
+
+
+class Solution:
+    def missingNumber(self, nums: List[int]) -> int:
+        nums.sort()
+        for i in range(len(nums)):
+            if nums[i] != i:
+                return i
+
+        return len(nums)
+
+
+t = Solution()
+print(t.missingNumber([0, 1, 3]))
