0a0b8ec6507f28f333bd18ffc000cbdb27c45913
think101 <taohuayuan2000@hotmail.com>
2022-04-11 20:36:25 -0700

    208. Implement Trie (Prefix Tree)

---
+++ src/implement_trie.py
@@ -0,0 +1,45 @@
+class TrieNode:
+
+    def __init__(self):
+        self.children = {}
+        self.is_word = False
+
+
+class Trie:
+
+    def __init__(self):
+        self.root = TrieNode()
+
+    def insert(self, word: str) -> None:
+        cur = self.root
+
+        for c in word:
+            if c not in cur.children:
+                cur.children[c] = TrieNode()
+            cur = cur.children[c]
+        cur.is_word = True
+
+    def search(self, word: str) -> bool:
+        cur = self.root
+
+        for c in word:
+            if c not in cur.children:
+                return False
+            cur = cur.children[c]
+        return cur.is_word
+
+    def startsWith(self, prefix: str) -> bool:
+        cur = self.root
+
+        for c in prefix:
+            if c not in cur.children:
+                return False
+            cur = cur.children[c]
+        return True
+
+
+t = Trie()
+t.insert("apple")
+print(t.search("apple"))
+print(t.search("app"))
+print(t.startsWith("app"))
