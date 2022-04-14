17e523a920d93c8db87aa728956c25b8508a36fe
think101 <taohuayuan2000@hotmail.com>
2022-04-13 23:41:39 -0700

    212. Word Search II draft implement with Trie barely beat TLE

--- src/word_search_2_trie.py
+++ src/word_search_2_trie.py
@@ -20,20 +20,6 @@ class WordDictionary:
             cur = cur.children[c]
         cur.is_word = True
 
-    # 0 not exists, 1 str exists as pref, 2 str exists as word
-    def search(self, w: str) -> int:
-        cur = self.root
-        for i in range(len(w)):
-            if w[i:i + 1] not in cur.children:
-                return 0
-            else:
-                cur = cur.children[w[i:i + 1]]
-                if i == len(w) - 1:
-                    if cur.is_word:
-                        return 2  # word
-                    else:
-                        return 1  # prefix
-
 
 class Solution:
     def findWords(self, board: List[List[str]], words: List[str]) -> List[str]:
@@ -41,37 +27,29 @@ class Solution:
         for w in words:
             wd.addWord(w)
 
-        res = set()
+        res, visited = set(), set()
         row, col = len(board), len(board[0])
 
-        def copyVisited(visited):
-            new_v = [[0] * col for _ in range(row)]
-            for a in range(row):
-                for b in range(col):
-                    new_v[a][b] = visited[a][b]
-            return new_v
+        def dfs(i, j, str, cur):
+            if i < 0 or i >= row or j < 0 or j >= col or (i, j) in visited or board[i][j] not in cur.children:
+                return
 
-        def dfs(visited, i, j, str):
-            if i < 0 or i >= row or j < 0 or j >= col or visited[i][j] == 1:
-                return False
             str += board[i][j]
-            r = wd.search(str)
-            if r == 0:
-                return False
-            elif r == 2:
+            cur = cur.children[board[i][j]]
+            visited.add((i, j))
+            if cur.is_word:
                 res.add(str)
 
-            # prefix or word
-            visited[i][j] = 1
-            dfs(copyVisited(visited), i - 1, j, str)
-            dfs(copyVisited(visited), i + 1, j, str)
-            dfs(copyVisited(visited), i, j - 1, str)
-            dfs(copyVisited(visited), i, j + 1, str)
+            dfs(i + 1, j, str, cur)
+            dfs(i - 1, j, str, cur)
+            dfs(i, j + 1, str, cur)
+            dfs(i, j - 1, str, cur)
+
+            visited.remove((i, j))
 
         for i in range(row):
             for j in range(col):
-                v = [[0] * col for _ in range(row)]
-                dfs(v, i, j, "")
+                dfs(i, j, "", wd.root)
 
         return list(res)
 
@@ -79,6 +57,6 @@ class Solution:
 t = Solution()
 # board = [["o", "a"], ["e", "t"]]
 # words = ["oa", "oet", "oat", "et", "etao", "tao", "abc"]
-board = [["a", "b", "c"], ["a", "e", "d"], ["a", "f", "g"]]
-words = ["abcdefg", "gfedcbaaa", "eaabcdgfa", "befa", "dgc", "ade"]
+board = [["a", "b", "c", "e"], ["z", "z", "d", "z"], ["z", "z", "c", "z"], ["z", "a", "b", "z"]]
+words = ["abcdce"]
 print(t.findWords(board, words))
