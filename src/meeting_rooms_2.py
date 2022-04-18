9c2ce1d0bc9be96e8e6fd8fdce813ad8e896bbf3
think101 <taohuayuan2000@hotmail.com>
2022-04-18 00:26:54 -0700

    253. Meeting Rooms II quick solution

--- src/meeting_rooms_2.py
+++ src/meeting_rooms_2.py
@@ -2,6 +2,29 @@ from typing import List
 
 
 class Solution:
+    def minMeetingRooms(self, intervals: List[List[int]]) -> int:
+        start = sorted([i[0] for i in intervals])
+        end = sorted([i[1] for i in intervals])
+
+        i, j = 0, 0
+        res, cur = 0,0
+
+        while i < len(intervals) and j < len(intervals):
+            if start[i] < end[j]:
+                cur += 1
+                i += 1
+                res = max(res, cur)
+            elif start[i] > end[j]:
+                j += 1
+                cur -= 1
+            else:
+                j += 1
+                i += 1
+
+        # it's possible there are still end time points, not processing them is fine
+
+        return res
+
     def minMeetingRooms_Slow(self, intervals: List[List[int]]) -> int:
         intervals.sort()
 
@@ -49,4 +72,6 @@ class Solution:
 
 t = Solution()
 print(t.minMeetingRooms_Slow([[0, 30], [5, 10], [15, 20]]))
+print(t.minMeetingRooms([[0, 30], [5, 10], [15, 20]]))
 print(t.minMeetingRooms_Slow([[1, 5], [8, 9], [8, 9]]))
+print(t.minMeetingRooms([[1, 5], [8, 9], [8, 9]]))
