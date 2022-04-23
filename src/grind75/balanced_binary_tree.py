118db0f431e124ab43115341b63591df323f892c
think101 <taohuayuan2000@hotmail.com>
2022-04-23 16:45:48 -0700

    grind75 week 2 110. Balanced Binary Tree O(N) solution

--- src/grind75/balanced_binary_tree.py
+++ src/grind75/balanced_binary_tree.py
@@ -11,6 +11,23 @@ class TreeNode:
 class Solution:
     def isBalanced(self, root: Optional[TreeNode]) -> bool:
 
+        def helper(root):
+            if not root:
+                return [True, 0]
+
+            l = helper(root.left)
+            r = helper(root.right)
+
+            if not l[0] or not r[0]:
+                return [False, -1]
+
+            return [abs(l[1] - r[1]) <= 1, max(l[1], r[1]) + 1]
+
+        return helper(root)[0]
+
+
+def isBalanced_ONLGN(self, root: Optional[TreeNode]) -> bool:
+
         def depth(root):
             if not root:
                 return 0
