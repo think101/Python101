8e307a3139047a2b7f9676b4abfbf58bab8d5f89
think101 <taohuayuan2000@hotmail.com>
2022-04-22 18:44:20 -0700

    grind75 week 2 542. 01 Matrix fix

--- src/grind75/01_matrix.py
+++ src/grind75/01_matrix.py
@@ -4,34 +4,30 @@ from typing import List
 class Solution:
     def updateMatrix(self, mat: List[List[int]]) -> List[List[int]]:
         row, col = len(mat), len(mat[0])
-
-        def copyVisited(visited):
-            newVisited = [[0] * col for _ in range(row)]
-            for i in range(row):
-                for j in range(col):
-                    newVisited[i][j] = visited[i][j]
-            return newVisited
-
-        def dfs(r, c, step, visited):
-            if r < 0 or r >= row or c < 0 or c >= col or visited[r][c] == 1:
-                return
-
-            visited[r][c] = 1
-            if mat[r][c] == 1:
-                mat[r][c] = step + 1
-            else:
-                mat[r][c] = min(mat[r][c], step + 1)
-
-            dfs(r - 1, c, step + 1, copyVisited(visited))
-            dfs(r + 1, c, step + 1, copyVisited(visited))
-            dfs(r, c + 1, step + 1, copyVisited(visited))
-            dfs(r, c - 1, step + 1, copyVisited(visited))
+        queue = []
 
         for i in range(row):
             for j in range(col):
-                if mat[i][j] == 0:
-                    visited = [[0] * col for _ in range(row)]
-                    dfs(i, j, 0, visited)
+                if mat[i][j] == 1:
+                    mat[i][j] = -1
+                else:
+                    queue.append((i, j))
+
+        while queue:
+            i, j = queue.pop(0)
+
+            if i - 1 >= 0 and mat[i-1][j] == -1:
+                mat[i-1][j] = mat[i][j] + 1
+                queue.append((i-1, j))
+            if i + 1 < row and mat[i+1][j] == -1:
+                mat[i+1][j] = mat[i][j] + 1
+                queue.append((i+1, j))
+            if j - 1 >= 0 and mat[i][j-1] == -1:
+                mat[i][j-1] = mat[i][j] + 1
+                queue.append((i, j-1))
+            if j + 1 < col and mat[i][j+1] == -1:
+                mat[i][j+1] = mat[i][j] + 1
+                queue.append((i, j+1))
 
         return mat
 
