c912271e4e9bd22bba4467e2a87086a5709d6acc
think101 <taohuayuan2000@hotmail.com>
2022-04-27 21:27:53 -0700

    grind75 week 3 70. Climbing Stairs

---
+++ src/grind75/climging_stairs.py
@@ -0,0 +1,14 @@
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        dp = [0 for _ in range(n+1)]
+        dp[0], dp[1] = 1, 1
+
+        for i in range(2, n+1):
+            dp[i] = dp[i-1] + dp[i-2]
+
+        return dp[n]
+
+
+t = Solution()
+print(t.climbStairs(3))
+print(t.climbStairs(4))
