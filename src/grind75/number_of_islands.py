d31c2abf2768c019afaa3ce7724409bfb37a0c1d
think101 <taohuayuan2000@hotmail.com>
2022-04-28 21:54:06 -0700

    grind75 week 4 200. Number of Islands fix bugs

--- src/grind75/number_of_islands.py
+++ src/grind75/number_of_islands.py
@@ -8,19 +8,20 @@ class Solution:
 
         for i in range(row):
             for j in range(col):
-                if grid[i][j] == 1:
-                    ones.append[[i, j]]
+                if grid[i][j] == "1":
+                    ones.append([i, j])
 
         def bfs(one):
+            i, j = one[0], one[1]
             res = 0
-            if grid[one[0], one[1]] == 1:
+            if grid[i][j] == "1":
                 res += 1
-                grid[one[0], one[1]] = 0
+                grid[i][j] = "0"
 
-                bfs([one[0]-1, one[1]]) if one[0]-1 >= 0 else None
-                bfs([one[0]+1, one[1]]) if one[0]+1 < row else None
-                bfs([one[0], one[1]-1]) if one[1]-1 >= 0 else None
-                bfs([one[0], one[1]+1]) if one[1]+1 < col else None
+                bfs([i-1, j]) if i-1 >= 0 else None
+                bfs([i+1, j]) if i+1 < row else None
+                bfs([i, j-1]) if j-1 >= 0 else None
+                bfs([i, j+1]) if j+1 < col else None
             return res
 
         return sum(bfs(one) for one in ones)
@@ -32,5 +33,13 @@ if __name__ == "__main__":
         ["1", "1", "0", "1", "0"],
         ["1", "1", "0", "0", "0"],
         ["0", "0", "0", "0", "0"]
+    ]
+    print(Solution().numIslands(grid))
+
+    grid = [
+        ["1", "1", "0", "0", "0"],
+        ["1", "1", "0", "0", "0"],
+        ["0", "0", "1", "0", "0"],
+        ["0", "0", "0", "1", "1"]
     ]
     print(Solution().numIslands(grid))
