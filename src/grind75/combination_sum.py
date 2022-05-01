bddc88e12f18354e249e56ce951bf203f36b7be1
think101 <taohuayuan2000@hotmail.com>
2022-05-01 09:48:31 -0700

    grind75 week 4 39. Combination Sum add solution

--- src/grind75/combination_sum.py
+++ src/grind75/combination_sum.py
@@ -2,7 +2,7 @@ from typing import List
 
 
 class Solution:
-    def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
+    def combinationSum_Mine(self, candidates: List[int], target: int) -> List[List[int]]:
         dp = [[] for _ in range(target + 1)]
 
         for i in range(1, target + 1):
@@ -21,6 +21,32 @@ class Solution:
 
         return dp[target]
 
+    def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
+        candidates.sort()
+        res = []
+
+        def dfs(target, i, cur):
+            if target == 0:
+                res.append(cur.copy())  # need to have a copy here
+                return
+
+            for j in range(i, len(candidates)):
+                if candidates[j] > target:
+                    break
+                cur.append(candidates[j])
+                dfs(target - candidates[j], j, cur)
+                cur.pop(-1)
+
+        dfs(target, 0, [])
+
+        return res
+
+
 
 t = Solution()
 print(t.combinationSum([2, 3, 6, 7], 7))
+print(t.combinationSum([2, 3, 5], 8))
+print(t.combinationSum([2, 3, 5], 7))
+print(t.combinationSum([2, 3, 5], 6))
+print(t.combinationSum([2, 3, 5], 1))
+
