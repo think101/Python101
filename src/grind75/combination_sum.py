4f7d9c98810ef41395900aaee37b0f308c9d6ecd
think101 <taohuayuan2000@hotmail.com>
2022-04-30 21:00:17 -0700

    grind75 week 4 39. Combination Sum

---
+++ src/grind75/combination_sum.py
@@ -0,0 +1,26 @@
+from typing import List
+
+
+class Solution:
+    def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
+        dp = [[] for _ in range(target + 1)]
+
+        for i in range(1, target + 1):
+            res = []
+            for j in candidates:
+                if i == j:
+                    res.append([j])
+                elif i - j > 0 and dp[i - j]:
+                    for l in dp[i - j]:
+                        if l:
+                            ll = sorted(l + [j])
+                            if ll not in res:
+                                res.append(ll)
+
+            dp[i] = res
+
+        return dp[target]
+
+
+t = Solution()
+print(t.combinationSum([2, 3, 6, 7], 7))
