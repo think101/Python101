998ef41071f327e03ce60ef4b36d3e30eeceaf0b
think101 <taohuayuan2000@hotmail.com>
2022-05-02 23:28:02 -0700

    grind75 week 4 46. Permutations dfs2

--- src/grind75/permutations.py
+++ src/grind75/permutations.py
@@ -21,13 +21,26 @@ class Solution:
         dfs(0, len(nums), [False] * len(nums), [])
         return res
 
-    def permute(self, nums: List[int]) -> List[List[int]]:
+    def permute_dfs2(self, nums: List[int]) -> List[List[int]]:
+        res = []
+
+        def dfs(nums, curr):
+            if not nums:
+                res.append(curr)
+
+            for i in range(len(nums)):
+                dfs(nums[:i] + nums[i + 1:], curr + [nums[i]])
+
+        dfs(nums, [])
+        return res
+
+    def permute_recursion(self, nums: List[int]) -> List[List[int]]:
         if len(nums) <= 1:
             return [nums]
 
         res = []
         for i in range(len(nums)):
-            t = self.permute(nums[:i] + nums[i+1:])
+            t = self.permute_recursion(nums[:i] + nums[i + 1:])
             for x in t:
                 res.append([nums[i]] + x)
 
@@ -36,4 +49,4 @@ class Solution:
 
 if __name__ == "__main__":
     s = Solution()
-    print(s.permute([1, 2, 3]))
+    print(s.permute_dfs2([1, 2, 3]))
