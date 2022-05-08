b7b7b12c5066afb33b78b10586b32fb7b8701aaf
think101 <taohuayuan2000@hotmail.com>
2022-05-07 22:15:22 -0700

    grind75 week 4 75. Sort Colors dutch partitioning solution

--- src/grind75/sort_colors.py
+++ src/grind75/sort_colors.py
@@ -3,7 +3,24 @@ from typing import List
 
 
 class Solution:
+
     def sortColors(self, nums: List[int]) -> None:
+        red, white, blue = 0, 0, len(nums) - 1
+
+        while white <= blue:
+            if nums[white] == 0:
+                nums[white], nums[red] = nums[red], nums[white]
+                white += 1
+                red += 1
+            elif nums[white] == 1:
+                white += 1
+            else:
+                nums[white], nums[blue] = nums[blue], nums[white]
+                blue -= 1
+
+        print(red, white, blue)
+
+    def sortColors_hmmm(self, nums: List[int]) -> None:
         """
         Do not return anything, modify nums in-place instead.
         """
