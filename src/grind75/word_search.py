12cb4ea94750bd6c0dfef99dde76452814e8be0d
think101 <taohuayuan2000@hotmail.com>
2022-05-11 22:33:54 -0700

    grind75 week 4 79. Word Search fix bug, at the edge of TLE

--- src/grind75/word_search.py
+++ src/grind75/word_search.py
@@ -6,16 +6,17 @@ class Solution:
         row, col = len(board), len(board[0])
 
         def dfs(i, j, index, path: List[str]):
-            if i < 0 or i >= row or j < 0 or j >= col or [i,j] in path or board[i][j] != word[index]:
+            if i < 0 or i >= row or j < 0 or j >= col or [i, j] in path or board[i][j] != word[index]:
                 return False
 
             if index == len(word) - 1:
                 return True
 
             path.append([i, j])
-            print(path)
-            if dfs(i-1, j, index+1, path) or dfs(i+1, j, index+1, path) or dfs(i, j-1, index+1, path) or dfs(i, j+1, index+1, path) :
+            if dfs(i - 1, j, index + 1, path) or dfs(i + 1, j, index + 1, path) or \
+                    dfs(i, j - 1, index + 1, path) or dfs(i, j + 1, index + 1, path):
                 return True
+            path.pop()  # backtrack to the previous state
 
             return False
 
@@ -26,3 +27,17 @@ class Solution:
                         return True
 
         return False
+
+
+if __name__ == "__main__":
+    board = [["A", "B", "C", "E"],
+             ["S", "F", "C", "S"],
+             ["A", "D", "E", "E"]]
+    word = "ABCCED"
+    print(Solution().exist(board, word))
+
+    board = [["A", "B", "C", "E"],
+             ["S", "F", "E", "S"],
+             ["A", "D", "E", "E"]]
+    word = "ABCESEEEFS"
+    print(Solution().exist(board, word))
