464a7b34243af968c5a3055270b44b6229546686
think101 <taohuayuan2000@hotmail.com>
2022-05-12 21:02:10 -0700

    grind75 week 4 33. Search in Rotated Sorted Array delete unnecessary method

--- src/grind75/search_in_rotated_sorted_array.py
+++ src/grind75/search_in_rotated_sorted_array.py
@@ -5,17 +5,6 @@ class Solution:
     def search(self, nums: List[int], target: int) -> int:
         l, r = 0, len(nums) - 1
 
-        def b_search(l, r, target):
-            while l <= r:
-                m = (l + r) // 2
-                if nums[m] == target:
-                    return m
-                if nums[m] < target:
-                    l = m + 1
-                else:
-                    r = m - 1
-            return -1
-
         # decide which part middle belongs to
         while l <= r:
             m = (l + r) // 2
@@ -25,12 +14,12 @@ class Solution:
 
             if nums[m] >= nums[l]:
                 if nums[l] <= target < nums[m]:  # left part
-                    return b_search(l, m - 1, target)
+                    r = m - 1
                 else:
                     l = m + 1
             elif nums[m] <= nums[r]:
                 if nums[r] >= target > nums[m]:  # right part
-                    return b_search(m + 1, r, target)
+                    l = m + 1
                 else:
                     r = m - 1
 
