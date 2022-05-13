ff33ba94ec9759beda14bfbd0b0811377aea5116
think101 <taohuayuan2000@hotmail.com>
2022-05-12 20:09:41 -0700

    grind75 week 4 104. Maximum Depth of Binary Tree

---
+++ src/grind75/maximum_depth_of_binary_tree.py
@@ -0,0 +1,36 @@
+from typing import Optional
+
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+class Solution:
+    def maxDepth(self, root: Optional[TreeNode]) -> int:
+        if not root:
+            return 0
+
+        return max(self.maxDepth(root.left), self.maxDepth(root.right)) + 1
+
+
+if __name__ == "__main__":
+    root = TreeNode(1)
+    root.left = TreeNode(2)
+    root.right = TreeNode(3)
+    root.left.left = TreeNode(4)
+    root.left.right = TreeNode(5)
+    root.right.left = TreeNode(6)
+    root.right.right = TreeNode(7)
+    root.left.left.left = TreeNode(8)
+    root.left.left.right = TreeNode(9)
+    root.left.right.left = TreeNode(10)
+    root.left.right.right = TreeNode(11)
+    root.right.left.left = TreeNode(12)
+    root.right.left.right = TreeNode(13)
+    root.right.right.left = TreeNode(14)
+    root.right.right.right = TreeNode(15)
+
+    solution = Solution()
+    print(solution.maxDepth(root))
