7c7de6ee3dc30eecece537e03cd9b38de7b65abf
think101 <taohuayuan2000@hotmail.com>
2022-05-12 22:05:13 -0700

    grind75 week 4 438. Find All Anagrams in a String fix TLE solution

--- src/grind75/find_all_anagrams_in_str.py
+++ src/grind75/find_all_anagrams_in_str.py
@@ -4,6 +4,45 @@ from typing import List
 
 class Solution:
     def findAnagrams(self, s: str, p: str) -> List[int]:
+        if len(s) < len(p):
+            return []
+
+        l = len(p)
+        d = collections.Counter(p)
+
+        def isAnagram(d1, d2):
+            if len(d1) != len(d2):
+                return False
+
+            for k in d1:
+                if k not in d2 or d1[k] != d2[k]:
+                    return False
+            return True
+
+        res = []
+        di = {}
+        for i in range(l):
+            if s[i] not in di:
+                di[s[i]] = 0
+            di[s[i]] += 1
+        if isAnagram(di, d):
+            res.append(0)
+
+        for i in range(l, len(s)):
+            if s[i] not in di:
+                di[s[i]] = 0
+            di[s[i]] += 1
+
+            di[s[i-l]] -= 1
+            if di[s[i-l]] == 0:
+                di.pop(s[i-l], None)
+
+            if isAnagram(di, d):
+                res.append(i-l+1)
+
+        return res
+
+    def findAnagrams_TLE(self, s: str, p: str) -> List[int]:
         l = len(p)
         d = collections.Counter(p)
 
