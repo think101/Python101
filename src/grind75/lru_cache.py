0e1f517e371c4d5214b7521e0c8349ef9b520b55
think101 <taohuayuan2000@hotmail.com>
2022-05-18 00:04:33 -0700

    grind75 week 4 146. LRU Cache mid term version

--- src/grind75/lru_cache.py
+++ src/grind75/lru_cache.py
@@ -13,17 +13,14 @@ class LRUCache:
         self.left = None
         self.right = None
 
-    def get(self, key: int) -> int:
-        if not self.d or key not in self.d:
-            return -1
-
+    def update_lru(self, key: int):
         t = self.left
         while t and t.key != key:
             t = t.next
 
         if t.prev:
             t.prev.next = t.next
-        if t.next:
+        if t.next and t.next != self.right:
             t.next.prev = t.prev
 
         self.right.next = t
@@ -31,16 +28,32 @@ class LRUCache:
         t.next = None
         self.right = t
 
+    def get(self, key: int) -> int:
+        if not self.d or key not in self.d:
+            return -1
+
+        self.update_lru(key)
+
         return self.d[key]
 
     def put(self, key: int, value: int) -> None:
         if key in self.d:
             self.d[key] = value
+            self.update_lru(key)
 
         if len(self.d) < self.c:
             self.d[key] = value
+
+            if not self.left:
+                self.left = Node(key, value)
+                self.right = self.left
+            else:
+                self.right.next = Node(key, value)
+                self.right.next.prev = self.right
+                self.right = self.right.next
         else:
             self.d.pop(self.left.key, None)
+            self.d[key] = value
 
             self.right.next = Node(key, value)
             self.right.next.prev = self.right
@@ -58,4 +71,6 @@ t = LRUCache(2)
 t.put(1, 1)
 t.put(2, 2)
 print(t.get(1))
+t.put(3, 3)
+print(t.get(2))
 
