aa74ee536ed75e3ad5fb7c7b8189f0472e7679bd
think101 <taohuayuan2000@hotmail.com>
2022-05-24 21:21:19 -0700

    grind75 week 8 23. Merge k Sorted Lists draft version

---
+++ src/grind75/merge_k_sorted_lists.py
@@ -0,0 +1,46 @@
+from typing import List, Optional
+
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+class Solution:
+    def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
+        l = len(lists)
+
+        dummy = ListNode(0)
+        c_node = dummy
+
+        while True:
+            current = -1
+            for i in range(l):
+                if lists[i]:
+                    if current < 0 or lists[i].val < lists[current].val:
+                        current = i
+
+            print(current)
+            if current > 0:
+                c_node.next = lists[current]
+                lists[current] = lists[current].next
+                c_node = c_node.next
+                c_node.next = None
+            else:
+                break
+
+        return dummy.next
+
+
+if __name__ == "__main__":
+    s = Solution()
+    l1 = ListNode(1)
+    l1.next = ListNode(4)
+    l1.next.next = ListNode(5)
+    l2 = ListNode(1)
+    l2.next = ListNode(3)
+    l2.next.next = ListNode(4)
+    l3 = ListNode(2)
+    l3.next = ListNode(6)
+    l = [l1, l2, l3]
+    print(s.mergeKLists(l))
