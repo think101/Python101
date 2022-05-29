d5322759be3cffd5df46ed208ba9685307fefc3a
think101 <taohuayuan2000@hotmail.com>
2022-05-28 22:33:19 -0700

    220. Contains Duplicate III minor change

--- src/contains_duplicate_iii.py
+++ src/contains_duplicate_iii.py
@@ -14,7 +14,7 @@ class Solution:
             sl.add(nums[i])
             ind = sl.index(nums[i])
 
-            if (ind - 1 >=0 and abs(sl[ind-1] - sl[ind]) <= t) or (ind + 1 < len(sl) and abs(sl[ind+1] - sl[ind]) <= t):
+            if (ind - 1 >= 0 and abs(sl[ind-1] - sl[ind]) <= t) or (ind + 1 < len(sl) and abs(sl[ind+1] - sl[ind]) <= t):
                 return True
 
         return False
@@ -25,5 +25,3 @@ print(t.containsNearbyAlmostDuplicate([-3, 3, -6], 2, 3))
 print(t.containsNearbyAlmostDuplicate([1, 0, 1, 1], 1, 2))
 print(t.containsNearbyAlmostDuplicate([1, 5, 9, 1, 5, 9], 2, 3))
 print(t.containsNearbyAlmostDuplicate([1, 2, 5, 6, 7, 2, 4], 4, 0))
-
-
