90e90e2a30374ac4309a5fddced79dbe02ec62a6
think101 <taohuayuan2000@hotmail.com>
2022-06-09 15:28:38 -0700

    2055. Plates Between Candles initial version

---
+++ src/grind75/plates_between_candles.py
@@ -0,0 +1,29 @@
+from bisect import bisect
+from typing import List
+
+
+class Solution:
+    def platesBetweenCandles(self, s: str, queries: List[List[int]]) -> List[int]:
+        candles = []
+
+        for i in range(len(s)):
+            if s[i] == '|':
+                candles.append(i)
+
+        res = [0 for i in range(len(queries))]
+        for i in range(len(queries)):
+            left = bisect(candles, queries[i][0] - 1)
+            right = bisect(candles, queries[i][1]) - 1
+
+            if candles[right] - candles[left] <= 1:
+                res[i] = 0
+                continue
+
+            res[i] = candles[right] - candles[left] - 1 - (right - left - 1)
+
+        return res
+
+
+if __name__ == "__main__":
+    s = Solution()
+    print(s.platesBetweenCandles("**|**|***|", [[2, 5], [5, 9]]))
