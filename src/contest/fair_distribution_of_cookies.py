451a3b937aa5fc5ffa5b67b24f31766b0600839c
think101 <taohuayuan2000@hotmail.com>
2022-06-14 00:38:07 -0700

    2305. Fair Distribution of Cookies TLE

---
+++ src/contest/fair_distribution_of_cookies.py
@@ -0,0 +1,26 @@
+from typing import List
+
+
+class Solution:
+    def distributeCookies(self, cookies: List[int], k: int) -> int:
+
+        def dfs(cookies, i, sums, res):
+            if i == len(cookies):
+                res[0] = min(res[0], max(sums))
+                return
+
+            for j in range(len(sums)):
+                sums[j] += cookies[i]
+                dfs(cookies, i + 1, sums, res)
+                sums[j] -= cookies[i]
+
+        res = [float('inf')]
+        dfs(cookies, 0, [0] * k, res)
+        return res[0]
+
+
+if __name__ == "__main__":
+    s = Solution()
+    print(s.distributeCookies([8, 15, 10, 20, 8], 2))
+    print(s.distributeCookies([6, 1, 3, 2, 2, 4, 1, 2], 3))
+    print(s.distributeCookies([76265, 7826, 16834, 63341, 68901, 58882, 50651, 75609], 8))
