e8a2f2c038560d12b0601349b44cb8bcb6de9b09
think101 <taohuayuan2000@hotmail.com>
2022-07-05 21:42:07 -0700

    131. Palindrome Partitioning fix bug

--- src/patterns/backtrack/palindrome_partitioning.py
+++ src/patterns/backtrack/palindrome_partitioning.py
@@ -3,28 +3,37 @@ from typing import List
 
 class Solution:
     def partition(self, s: str) -> List[List[str]]:
-        d = {}     # dict with key as i, and value as the result of s[i:]
+        d = {}  # dict with key as i, and value as the result of s[i:]
 
         def helper(s):
             if len(s) == 1:
-                d.put(len(s), [[s]])
+                d[len(s)] = [[s]]
                 return [[s]]
 
             current_res = []
             for i in range(1, len(s)):
-                r = d[len(s) - i]
-                first_s = s[:1] + r[0][0]
-                if first_s == first_s[::-1]:
-                    current_res.append([first_s] + r[1:])
+                t = s[i:]
+                if len(t) not in d:
+                    helper(t)
 
-            d.put(len(s), current_res)
+                r = d[len(s) - i]
+                for l in r:
+                    first_s = s[:i]
+                    if first_s == first_s[::-1]:
+                        current_res.append([first_s] + l)
+
+            d[len(s)] = current_res
 
         helper(s)
         return d.get(len(s))
 
 
-
 if __name__ == "__main__":
     s = Solution()
-    print(s.partition([1, 2, 3]))
+    print(s.partition("aab"))
+    print(s.partition("a"))
+    print(s.partition("aa"))
+    print(s.partition("ab"))
+    print(s.partition("abc"))
+    print(s.partition("abcdcc"))
 
