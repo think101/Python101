e9b481f079197b9ce0c8f06c2642604d10dbca62
think101 <taohuayuan2000@hotmail.com>
2022-09-14 21:12:47 -0700

    784. Letter Case Permutation

---
+++ src/patterns/backtrack/letter_case_permutation.py
@@ -0,0 +1,32 @@
+from typing import List
+
+
+class Solution:
+    def letterCasePermutation(self, s: str) -> List[str]:
+        res = []
+
+        def backtrack(visited, i):
+            if i == len(s):
+                res.append(''.join(visited))
+                return
+
+            if not s[i].isalpha():
+                visited.append(s[i])
+                backtrack(visited, i+1)
+                visited.pop()
+            else:
+                visited.append(s[i].lower())
+                backtrack(visited, i+1)
+                visited.pop()
+
+                visited.append(s[i].upper())
+                backtrack(visited, i+1)
+                visited.pop()
+
+        backtrack([], 0)
+        return res
+
+
+if __name__ == "__main__":
+    s = Solution()
+    print(s.letterCasePermutation("3z4"))
